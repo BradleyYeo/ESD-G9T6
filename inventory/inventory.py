7d80e8138509714640c31c2ebad704a7fa7564fb
tame12 <36912595+tame12@users.noreply.github.com>
2022-04-02 17:18:29 +0800

    inventory compose

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -5,7 +5,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'dbURL') or "mysql+mysqlconnector://root:@localhost:3306/inventory"
+    'dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
