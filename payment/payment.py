93b6d62fa5f1be3ad52f4a44f308f09976884777
blazefire710 <paul.soh.2020@sis.smu.edu.sg>
2022-04-05 08:46:08 +0800

    Payment cleanup n webhook bug fix

--- payment/payment.py
+++ payment/payment.py
@@ -3,7 +3,7 @@ import os
 
 import stripe
 from dotenv import load_dotenv  # this is to load the environment variables from the .env file!
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 
 load_dotenv()
 
@@ -21,19 +21,13 @@ stripe_keys = {
 
 stripe.api_key = stripe_keys["secret_key"]
 
-
 #############homepage where checkout button is shown#############
-# this should be redirected from the UI when the "checkout/payment button is clicked first"
+# this will be redirected from the UI when the "checkout/payment button is clicked first"
 orderdata = None
 @app.route('/payment', methods=['POST'])
 def checkout():
-    # data = request.get_json()
     data = request.get_data(as_text=True)
     print("data:", data)
-
-    # global orderdata
-    # orderdata = data
-    # print(request)
     # total_price = data["total_price"]
     # customer_email = data["customer_email"]
     # customer_id = data["customer_id"]
@@ -49,7 +43,6 @@ def charge():
     # # check for total price sent by complex MS
     # data = request.get_json()
     # total_price = data["total_price"]
-
     amount = 1000  # Amount in cents #currently hardcoded
 
     customer = stripe.Customer.create(
@@ -67,8 +60,7 @@ def charge():
     # redirects user to the payment success page
     return render_template('charge.html', amount=amount)
 
-
-###############webhook to confirm payment###############
+###############webhook route to confirm payment###############
 @app.route("/webhook", methods=["POST"])
 def stripe_webhook():
     payload = request.get_data(as_text=True)
@@ -79,9 +71,9 @@ def stripe_webhook():
             payload, sig_header, stripe_keys["endpoint_secret"]
         )
     except ValueError as e:
-        # invalid payload
-        print("Payment Failed, Invalid Payload")
-        return render_template('chargefailed1.html', amount=amount)
+        # Invalid payload error
+        # print("Payment Failed, Invalid Payload")
+        # return render_template('chargefailed1.html')
         return jsonify(
             {
                 "code": 400,
@@ -91,9 +83,9 @@ def stripe_webhook():
         )
 
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        # return "Invalid signature", 400 #this is the original one
-        ### ignore the jsonify return first since it seems to be returning somewhere else
+        # Invalid signature error
+        # print("Payment Failed, Invalid Signature")
+        # return render_template('chargefailed2.html')
         return jsonify(
             {
                 "code": 400,
@@ -101,27 +93,18 @@ def stripe_webhook():
                 "message": "Payment Failed, Invalid Signature"
             }
         )
-        print("Payment Failed, Invalid Signature")
-        return render_template('chargefailed2.html', amount=amount)
-
-        # Handle the checkout.session.completed event
-
-    ### the two lines below are from the other version of stripe's API, so "checkout.session.completed doesn't work" replace with charge.succeeded
-    # if event["type"] == "checkout.session.completed":
-    #     print("Payment was successful123")
 
     if event["type"] == "charge.succeeded":
-        print("Payment Charge Confirmed")  # as long as this prints, the webhook is working properly.
-        ### ignore the jsonify return first since it seems to be returning somewhere else
-        # return jsonify(
-        #     {
-        #         "code": 200,
-        #         "data": {},
-        #         "message": "Payment Successful"
-        #     }
-        # )
-        # let me try to redirect to payment complex MS here / redirect back to UI which will call checkout complex MS
-        # return redirect(f"/checkout")  # trying to redirect to complex #try if it works
+        print("Payment Charge Confirmed")  # as long as this prints, the webhook is working properly. lookout for this to verify payment
+        # For future development: To implement more functionality when the the successful payment event is created.
+
+        return jsonify(
+            {
+                "code": 200,
+                "data": {},
+                "message": "Payment Successful"
+            }
+        )
 
     return jsonify(
         {
@@ -130,7 +113,6 @@ def stripe_webhook():
             "message": "Payment Successful"
         }
     )
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5069, debug=True)
--- payment/readME.txt
+++ payment/readME.txt
@@ -1,10 +1,14 @@
-- stripe.exe is the webhook command line app that must be run for the payment MS to track when a payment has been received successfully
+- stripe.exe is the webhook command line app that must be run for the payment MS to track when a payment has been received successfully. 
+ - It is included here so that the webhook command can be typed in the same directory itself
 - charge.html is a page template that is shown after user has made payment
 - checkout.html is a page template that is shown for user to click on the button to make payment
 - .env holds the API keys and webhook keys which will be retrieved from inside payment.py for authentication
 
 Steps
-1. Complex MS will do a redirection to @/payment to kickstart the payment process
+0. While inside the payment folder directory, run the stripe webhook by typing stripe listen --forward-to 127.0.0.1:5069/webhook into a command window.
+1. Click on the UI "Stripe checkout" button to do a redirection to @/payment to kickstart the payment process
 2. Checkout.html will be rendered for user to make payment
-3. After payment, charge.html will be rendered to show user that payment has been confirmed
-4. User will can click on the button to go back to the homepage
+3.1 After payment, charge.html will be rendered and shown to the user
+3.2 Alongside the payment, the webhook listener should display the events when the Stripe API is called. In particular, look out for a print statement "Payment Confirmed" together with the event "Charge.succeeded" to verify payment. 
+4.1 User will can click on the button to go back to the homepage
+4.2 At this point, charge.html would have routed back to the Complex Microservice to continue on with the ordering process.
